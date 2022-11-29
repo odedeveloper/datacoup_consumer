4d22cf3fb0c2d31b9ffd2f52f38fb7fd1fb8081b
AJ8454 <ajaypangare8454@gmail.com>
2022-11-29 14:30:53 +0530

    video rotate

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:datacoup/export.dart';
-import 'dart:io';
 
 class HomeScreen extends GetWidget<HomeController> {
   HomeScreen({super.key});
@@ -71,59 +70,58 @@ class AppBottomNavgationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Theme.of(context).appBarTheme.backgroundColor,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black,
-              blurRadius: 0.5,
-              spreadRadius: 0.5,
-              offset: Offset(0.3, 0.8),
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).appBarTheme.backgroundColor,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black45,
+                blurRadius: 0.5,
+                spreadRadius: 0.5,
+                offset: Offset(0.0, 0.01),
+              ),
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            bottomNavButtons(
+              onClicked: () => onIndexSelected(0),
+              iconData: FontAwesomeIcons.house,
+              color: controller.onIndexSelected.value == 0
+                  ? deepOrangeColor
+                  : Colors.grey,
             ),
-          ]),
-      padding: Platform.isAndroid
-          ? const EdgeInsets.only(top: 5, bottom: 2)
-          : const EdgeInsets.only(top: 5, bottom: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          bottomNavButtons(
-            onClicked: () => onIndexSelected(0),
-            iconData: FontAwesomeIcons.house,
-            color: controller.onIndexSelected.value == 0
-                ? deepOrangeColor
-                : Colors.grey,
-          ),
-          bottomNavButtons(
-            onClicked: () => onIndexSelected(1),
-            iconData: FontAwesomeIcons.solidHeart,
-            color: controller.onIndexSelected.value == 1
-                ? deepOrangeColor
-                : Colors.grey,
-          ),
-          bottomNavButtons(
-            onClicked: () => onIndexSelected(2),
-            iconData: FontAwesomeIcons.solidLightbulb,
-            color: controller.onIndexSelected.value == 2
-                ? deepOrangeColor
-                : Colors.grey,
-          ),
-          bottomNavButtons(
-            onClicked: () => onIndexSelected(3),
-            iconData: FontAwesomeIcons.play,
-            color: controller.onIndexSelected.value == 3
-                ? deepOrangeColor
-                : Colors.grey,
-          ),
-          bottomNavButtons(
-            onClicked: () => onIndexSelected(4),
-            iconData: FontAwesomeIcons.solidUser,
-            color: controller.onIndexSelected.value == 4
-                ? deepOrangeColor
-                : Colors.grey,
-          ),
-        ],
+            bottomNavButtons(
+              onClicked: () => onIndexSelected(1),
+              iconData: FontAwesomeIcons.solidHeart,
+              color: controller.onIndexSelected.value == 1
+                  ? deepOrangeColor
+                  : Colors.grey,
+            ),
+            bottomNavButtons(
+              onClicked: () => onIndexSelected(2),
+              iconData: FontAwesomeIcons.solidLightbulb,
+              color: controller.onIndexSelected.value == 2
+                  ? deepOrangeColor
+                  : Colors.grey,
+            ),
+            bottomNavButtons(
+              onClicked: () => onIndexSelected(3),
+              iconData: FontAwesomeIcons.play,
+              color: controller.onIndexSelected.value == 3
+                  ? deepOrangeColor
+                  : Colors.grey,
+            ),
+            bottomNavButtons(
+              onClicked: () => onIndexSelected(4),
+              iconData: FontAwesomeIcons.solidUser,
+              color: controller.onIndexSelected.value == 4
+                  ? deepOrangeColor
+                  : Colors.grey,
+            ),
+          ],
+        ),
       ),
     );
   }
